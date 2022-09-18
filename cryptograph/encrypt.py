26e80c95d63b13fd313a249741a8de5a37ca3874
Jasper Nathaniel J <95154891+Jappynathaniel@users.noreply.github.com>
2022-09-18 22:12:22 +0530

    Update encrypt.py

--- cryptograph/encrypt.py
+++ cryptograph/encrypt.py
@@ -1,10 +1,10 @@
 from rsa_python import rsa
 import decrypt.decrypt as dec
-import sys
+import sys 
 key_pair = rsa.generate_key_pair(1024)
 message = sys.argv[1]
-cipher = rsa.encrypt(message, key_pair["public"], key_pair["modulus"])
-decry = dec.decryptor(cipher, key_pair["private"], key_pair["modulus"])
+cipher = rsa.encrypt(message, key_pair["public"], key_pair["modulus"]) #The message to be encoded along with the public key.
+decry = dec.decryptor(cipher, key_pair["private"], key_pair["modulus"]) #Private key is used to decode the message.
 print("cypher",cipher)
 
 print("\nprivate",key_pair["private"])
